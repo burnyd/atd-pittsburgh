cd77c87a574f1116bbf5adf24c9ace5a4d8072d9
Daniel Hertzberg <burnyd@gmail.com>
2017-11-06 09:31:33 -0500

    Update eapi_find_neighbors.py

--- eapi_find_neighbors.py
+++ eapi_find_neighbors.py
@@ -5,11 +5,11 @@ from jsonrpclib import Server
 
 switchusername = 'arista'
 switchpassword = 'arista'
-switches = ['leaf3']
+switches = ['192.168.0.10']
 
 def find_neighbors(): #Function makes sure that each and every API is actually running on each switches and can pull with the current password.
     for switch in switches:
-        urlString = "http://%s:%s@%s/command-api" % (switchusername, switchpassword, switch)
+        urlString = "https://%s:%s@%s/command-api" % (switchusername, switchpassword, switch)
         switchReq = Server( urlString )
         response = switchReq.runCmds( 1, ["show lldp neighbors"] )
         neighborresponse = response[0]["lldpNeighbors"]
